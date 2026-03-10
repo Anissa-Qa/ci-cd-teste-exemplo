61ecd49decbd268322bf561bdb8ad4c3b3662735
Anissa <anissa.adv@gmail.com>
2026-03-10 12:52:52 -0300

    publicacao no gh pages

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require("cypress");
+const { allureCypress } = require ("allure-cypress/reporter");
 
 module.exports = defineConfig({
+  video: true,
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('@shelex/cypress-allure-plugin/writer')(on, config);
+      return config;
     },
   },
 });
+
